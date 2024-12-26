2ae0e02c5e3ecd22b73ff88c37f9c7d9696727f2
gelardrc <gabriel.guitar@gmail.com>
2024-12-26 19:06:20 -0300

    Upgrade for noetic

--- scripts/wave_gelo.py
+++ scripts/wave_gelo.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ## codigo de teste para wavefront ##
 import rospy
--- scripts/wavefront_a_star.py
+++ scripts/wavefront_a_star.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 ## codigo de teste para wavefront ##
 import rospy
